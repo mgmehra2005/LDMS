bac498a33ac46bfbd8c82e8cb26473c44895732a
MG <84391389+mgmehra2005@users.noreply.github.com>
2022-04-14 13:34:45 +0000

    Added Create table option

--- __functions__.py
+++ __functions__.py
@@ -1,6 +1,7 @@
 #Functions
 
 def clear():
+  '''clears all the text on the screen.'''
   import os
   try:
     os.system("cls")
@@ -25,11 +26,13 @@ def current_time(time_parameter):      #Parameters : full_time, hour, minute, no
     return result
 
 def logs(log):
+  '''Writes logs'''
   log_file=open(".logs", "a")
   log_file.write(f"=> {current_time('now')} : {log}\n")
   log_file.close
 
 def show_databse():
+  '''This functions shows all the databases present in the database directory.'''
   import os
   directory=os.listdir(path="Databases/")
   serial_number=0
@@ -37,3 +40,6 @@ def show_databse():
     serial_number+=1
     print(f"{serial_number}. {databases}")
 
+def style_reset():
+  from colorama import Style
+  print(Style.RESET_ALL)
--- main.py
+++ main.py
@@ -5,85 +5,118 @@ from tabulate import tabulate
 from functools import reduce
 import __functions__
 from __functions__ import logs
-import colorama
+from __functions__ import style_reset
+from colorama import Fore
 import os
 import time
 
 #Start
-user_choice="input taker"
-while (user_choice != 99):
-  try:
-    print("\n-> MENU\n")
-    options = ["Create Database",
-             "Show Databases",
-             "Delete Database",
-             "Select Database"]
-    option_number=0
-    for items in options:
-      option_number = option_number + 1
-      print(f"{option_number}. {items}")
-    print("99. To Exit")
+try:
+    user_choice = "input taker"
+    while (user_choice != 99):
+        print("\n-> MENU\n")
+        options = [
+            "Create Database", "Show Databases", "Delete Database",
+            "Create Table"
+        ]
+        option_number = 0
+        for items in options:
+            option_number = option_number + 1
+            print(f"{option_number}. {items}")
+        print("99. To Exit")
 
-    user_choice=int(input('\nChoose the option : '))      #User input
-    __functions__.clear()
-
-  #Conditions
-    if (user_choice==1):
-      print(f"\n>> Create Database")
-      db_name=input("\nDatabase Name : ")
-      db_availablity=os.path.isfile(f"Databases/{db_name}")
-      if db_availablity:
-        print(f"\nDatabase {db_name} is already Exist.")
-        logs("Database {db_name} is already Exist.")  
-      else:  
-        db_path=f"Databases/{db_name}"
-        create_db=open(db_path, "x")
-        create_db.close()
-        print("\nDatabase Created Successfully.")
-      logs(f"Database {db_name} created successfully.")
-      time.sleep(3)
-      __functions__.clear()
-      continue
-
-    elif (user_choice==2):
-      print(f"\n>> Databases\n")
-      __functions__.show_databse()
-      logs("Databases shown Successfully.")
-
-    elif (user_choice==3):
-      print(f"\n>> Dropping Database\n")
-      __functions__.show_databse()
-      db_name=input("\nDatabase name : ")
-      try:
-        os.system(f"rm Databases/{db_name}")
-        print("Database removed successfully.")
-        logs(f"Database {db_name} is removed successfully.")
-        time.sleep(3)
+        user_choice = int(input('\nChoose the option : '))  #User input
         __functions__.clear()
-        continue
-      except Exception as error:
-        print("\nThere is an eroor while removing database.")
-        user_input=input("\nDo you want to see the error (y/n) : ")
-        if (user_input=="y"):
-          print(error)
-          time.sleep(5)
-          break
-        elif (user_input=="n"):
-          print("\nSorry for the interruption.")
-        logs(f"Error -> {error}")
-        break
 
-  except Exception as program_error:
-    print("\nThere is an eroor while running the program.")
-    user_input=input("\nDo you want to see the error (y/n) : ")
-    if (user_input=="y"):
-      print(program_error)
-      time.sleep(5)
-      break
-    elif (user_input=="n"):
-      print("\nSorry for the interruption.")
+        #Conditions
+        if (user_choice == 1):
+            print(f"\n>> Create Database")
+            db_name = input("\nDatabase Name : ")
+            db_availablity = os.path.isfile(f"Databases/{db_name}")
+            if db_availablity:
+                print(Fore.RED + f"\nDatabase {db_name} is already Exist.")
+                style_reset()
+                logs("Database {db_name} is already Exist.")
+            else:
+                db_path = f"Databases/{db_name}"
+                create_db = open(db_path, "x")
+                create_db.close()
+                print(Fore.GREEN + "Database Created Successfully.")
+                style_reset()
+            logs(f"Database {db_name} created successfully.")
+            time.sleep(3)
+            __functions__.clear()
+            continue
+
+        elif (user_choice == 2):
+            print(f"\n>> Databases\n")
+            __functions__.show_databse()
+            logs("Databases shown Successfully.")
+            time.sleep(5)
+            __functions__.clear()
+            continue
+
+        elif (user_choice == 3):
+            print(f"\n>> Dropping Database\n")
+            __functions__.show_databse()
+            db_name = input("\nDatabase name : ")
+            try:
+                os.system(f"rm Databases/{db_name}")
+                print(Fore.GREEN + "Database removed successfully.")
+                style_reset()
+                logs(f"Database {db_name} is removed successfully.")
+                time.sleep(3)
+                __functions__.clear()
+                continue
+            except Exception as error:
+                print(Fore.RED +
+                      "\nThere is an eroor while removing database.")
+                style_reset()
+                user_input = input("\nDo you want to see the error (y/n) : ")
+                if (user_input == "y"):
+                    print(error)
+                    time.sleep(5)
+                    break
+                elif (user_input == "n"):
+                    print("\nSorry for the interruption.")
+                logs(f"Error -> {error}")
+                break
+
+        elif (user_choice == 4):
+            print(f"\n>> Create Table\n")
+            __functions__.show_databse()
+            select_database = input("\nEnter database name : ")
+            db_availablity = os.path.isfile(f"Databases/{select_database}")
+            if db_availablity:
+                selected_database = open(f"{select_database}", "a")
+                print(Fore.GREEN + "Database selected successfully.")
+                style_reset()
+                table_name=input("Enter table name : ")
+                selected_database.write(f"{table_name} = dict()")
+                print(Fore.GREEN + "Table created successfully.")
+                style_reset()
+                logs(f"Table {table_name} created successfully.")
+                __functions__.clear()
+            else:
+                print(Fore.RED +
+                      "Database not found.\nPlease recheck the database name.")
+                style_reset()
+                time.sleep(3)
+                break
+                logs(f"Error -> {error}")
+
+    print(Fore.YELLOW + "Thanks for using Local Database System.")
+    time.sleep(4)
+
+except Exception as program_error:
+    print(Fore.RED + "\nThere is an eroor while running the program.")
+    style_reset()
+    user_input = input("Do you want to see the error (y/n) : ")
+    if (user_input == "y"):
+        print(program_error)
+        time.sleep(5)
+    elif (user_input == "n"):
+        print("\nSorry for the interruption.")
     logs(f"Error -> {program_error}")
-    break
-print("Thanks for using Local Database System.")
-time.sleep(4)
+
 #End
